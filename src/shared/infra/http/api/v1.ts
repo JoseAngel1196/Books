f1a18d68f5cb69cda3387c2b6c3255665eaa204d
Jose Angel Hidalgo Rosa <joseangel1196@hotmail.com>
2020-09-02 20:05:00 -0400

    exposing the book router

--- src/shared/infra/http/api/v1.ts
+++ src/shared/infra/http/api/v1.ts
@@ -1,10 +1,15 @@
 // Imports
 import express from 'express';
 
+// App Imports
+import { bookRouter } from '../../../../modules/book/infra/routes';
+
 const v1Router = express.Router();
 
 v1Router.get('/', (req, res) => {
   return res.json({ message: "We're up! " });
 });
+
+v1Router.use('/books', bookRouter);
 
 export { v1Router };
